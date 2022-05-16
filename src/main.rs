976b4dd461352c3df334ae29a8e84f6e991ffda8
Luke Shay <shay.luke17@gmail.com>
2022-05-16 15:32:20 -0500

    refactor: added consts

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ mod utils;
 
 static UNREACHABLE: &str = "parser should ensure only valid subcommand names are used";
 
+const FLY: &str = "fly";
+const CONFIG: &str = "config";
+const NEW: &str = "new";
+const GEN: &str = "gen";
+const SCHEMA: &str = "schema";
+
+const FLY_JSON: &str = "fly.json";
+
 fn main() {
     let cmd = clap::Command::new(env!("CARGO_CRATE_NAME"))
         .bin_name(env!("CARGO_CRATE_NAME"))
@@ -10,17 +18,17 @@ fn main() {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
-            clap::command!("fly")
+            clap::command!(FLY)
                 .about("Used for fly related things")
                 .subcommand_required(true)
                 .arg_required_else_help(true)
                 .subcommand(
-                    clap::command!("config")
+                    clap::command!(CONFIG)
                         .about("Used for updating, manipulating, or getting configs")
                         .subcommand_required(true)
                         .arg_required_else_help(true)
                         .subcommands(vec![
-                            clap::command!("new")
+                            clap::command!(NEW)
                                 .about("Generates a new fly config file")
                                 .arg_required_else_help(true)
                                 .args(&[
@@ -41,17 +49,17 @@ fn main() {
                                         .short('o')
                                         .help("The name of the JSON config file")
                                         .allow_invalid_utf8(false)
-                                        .default_value("fly.json")
+                                        .default_value(FLY_JSON)
                                         .required(false),
                                 ]),
-                            clap::command!("gen")
+                            clap::command!(GEN)
                                 .about("Generates the fly.toml file")
                                 .args(&[
                                     clap::arg!(--"input-file" <FILE_NAME>)
                                         .short('i')
                                         .help("The name of the input JSON config file")
                                         .allow_invalid_utf8(false)
-                                        .default_value("fly.json")
+                                        .default_value(FLY_JSON)
                                         .required(false),
                                     clap::arg!(--"output-file" <FILE_NAME>)
                                         .short('o')
@@ -60,7 +68,7 @@ fn main() {
                                         .default_value("fly.toml")
                                         .required(false),
                                 ]),
-                            clap::command!("schema")
+                            clap::command!(SCHEMA)
                                 .about("Generates the fly config schema")
                                 .args(&[clap::arg!(--"file" <FILE_NAME>)
                                     .short('o')
@@ -73,15 +81,15 @@ fn main() {
         );
 
     let result = match cmd.get_matches().subcommand() {
-        Some(("fly", fly_matches)) => match fly_matches.subcommand() {
-            Some(("config", fly_config_matches)) => match fly_config_matches.subcommand() {
-                Some(("new", fly_config_new_matches)) => {
+        Some((FLY, fly_matches)) => match fly_matches.subcommand() {
+            Some((CONFIG, fly_config_matches)) => match fly_config_matches.subcommand() {
+                Some((NEW, fly_config_new_matches)) => {
                     cmds::fly_cmds::config_new(fly_config_new_matches)
                 }
-                Some(("gen", fly_config_gen_matches)) => {
+                Some((GEN, fly_config_gen_matches)) => {
                     cmds::fly_cmds::config_gen(fly_config_gen_matches)
                 }
-                Some(("schema", fly_config_schema_matches)) => {
+                Some((SCHEMA, fly_config_schema_matches)) => {
                     cmds::fly_cmds::config_schema(fly_config_schema_matches)
                 }
                 _ => unreachable!("{}", UNREACHABLE),
