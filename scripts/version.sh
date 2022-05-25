8341c94a11fa2fb4db2671c4b76d8883aa8a75da
Luke Shay <shay.luke17@gmail.com>
2022-05-25 09:26:10 -0500

    chore: fixed version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,4 +3,4 @@
 set -e
 
 sed -i.bak -E "s/^version = \"${1}\"$/version = \"${2}\"/" Cargo.toml
-sed -i.bak -E "s/\"version\": \"${1}\"$/\"version\": \"${2}\"/" npm/package.json
+sed -i.bak -E "s/\"version\": \"${1}\"/\"version\": \"${2}\"/" npm/package.json
