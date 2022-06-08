30d5f16c5f405980e9bc6ca61e4bfe3a570e4c17
Luke Shay <shay.luke17@gmail.com>
2022-06-08 10:39:27 -0500

    chore: remove test from tag script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-echo "Running acceptance tests"
-
-cd npm
-
-npm ci --no-progress --ignore-scripts
-
-cargo run -- js config
-
-npm run check
-
 current_version="$(./scripts/current_version.sh)"
 
 git tag "v${current_version}"
